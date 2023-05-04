3462ed0e80d02b2c68927005dc0b39b9c8e810e3
hskowron <106318969+hskowron@users.noreply.github.com>
2023-05-04 15:28:56 +0000

    ASDASDAS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update index destroy]
+  before_action :correct_user, only: %i[edit update]
+  before_action :admin_user, only: :destroy
+
   private
 
   def user_params
@@ -8,6 +12,26 @@ class UsersController < ApplicationController
       :password,
       :password_confirmation
     )
+  end
+
+  # Before filters
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    puts(">", current_user, "<")
+    redirect_to(root_url) unless current_user.admin?
   end
 
   public
@@ -30,5 +54,27 @@ class UsersController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,17 +27,27 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      @current_user = user if user && session[:session_token] == user.session_token
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
 
+  def current_user?(user)
+    user&.== current_user
+  end
+
   def logged_in?
     !current_user.nil?
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
